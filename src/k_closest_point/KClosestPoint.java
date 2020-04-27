9babbc1d3050c7f4435da24ab2fb05eccfa369d3
Vilarj <lamaravillasan@outlook.com>
2020-04-27 14:04:50 -0700

    K Closest Point

---
+++ src/k_closest_point/KClosestPoint.java
@@ -0,0 +1,65 @@
+package k_closest_point;
+
+import java.util.Arrays;
+
+/**
+ * Points = [(-2,4), (0, -2), (-1,0), (3,-5),
+ * (-2,-3), (3,2)]
+ *
+ * Problem: Giving these points, how would you find
+ * the closest point to the origin?
+ *
+ * Note: Origin = (0,0)
+ */
+public class KClosestPoint {
+    private static double distance;
+    private static final Plane2D ORIGIN = new Plane2D(0,0);
+
+    /**
+     * We will use the Pythagorean theorem to
+     * find the distances of each line
+     *
+     * @param point
+     * @return closest to ORIGIN
+     */
+    public static double PythagoreanTheorem (Plane2D point) {
+        double newX = point.getX() * point.getX();
+        double newY = point.getY() * point.getY();
+
+        distance = ( Math.sqrt(newX) + Math.sqrt(newY) );
+
+        return distance;
+    }
+
+    public static void main (String[] args) {
+        double[] distance = new double[10];
+        Plane2D[] point = new Plane2D[10];
+        double[] x = new double[10];
+        double[] y = new double[10];
+
+        for (int i = 0; i < point.length; i++) {
+            point[i] = new Plane2D(Math.round(Math.random() * 10), Math.round(Math.random() * 10));
+            x[i] = point[i].getX();
+            y[i] = point[i].getY();
+
+            System.out.print(point[i] + "\n");
+        }
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        System.out.println("");
+        for (int i = 0; i < x.length; i++) {
+            System.out.print("x" + (i+1) + " = " + x[i] + ", y" + (i+1) + " = " + y[i] + "\n");
+        }
+        System.out.println("");
+
+        for (int i = 0; i < distance.length; i++) {
+            distance[i] = Math.round(PythagoreanTheorem(point[i]));
+            System.out.print("Point " + (i + 1) + " is: " + distance[i] + "\n");
+        }
+
+        Arrays.sort(distance);
+        System.out.print("\nThe distance " + distance[0] + " is closest to " + ORIGIN);
+    }
+}
---
+++ src/k_closest_point/Plane2D.java
@@ -0,0 +1,31 @@
+package k_closest_point;
+
+public class Plane2D {
+    private double x, y;
+
+    public Plane2D (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX () {
+        return x;
+    }
+
+    public double getY () {
+        return y;
+    }
+
+    public void setX (double x) {
+        this.x = x;
+    }
+
+    public void setY (double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
